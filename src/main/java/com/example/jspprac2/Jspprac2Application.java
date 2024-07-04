37c159a50ff0f58a1cce508fd2b4632b863ef840
Murphy <murphytklee@gmail.com>
2024-07-04 14:04:05 +0900

    first commit

---
+++ src/main/java/com/example/jspprac2/Jspprac2Application.java
@@ -0,0 +1,13 @@
+package com.example.jspprac2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jspprac2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jspprac2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jspprac2/Jspprac2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jspprac2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jspprac2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
